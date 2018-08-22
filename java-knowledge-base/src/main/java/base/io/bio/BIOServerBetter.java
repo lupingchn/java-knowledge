3cafecf099ede6b5154416ad15fb884376f375a7
zhangluping <lupingchn@163.com>
2018-08-22 16:09:41 +0800

    上传文件

--- java-knowledge-base/src/main/java/base/io/bio/BIOServerBetter.java
+++ java-knowledge-base/src/main/java/base/io/bio/BIOServerBetter.java
@@ -3,12 +3,14 @@ package base.io.bio;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 /**
- * BIO服务端源码-同步阻塞
+ * BIO服务端源码__伪异步I/O
  * @author zhangluping on 2018/8/22.
  */
-public class BIOServer {
+public class BIOServerBetter {
     /**
      * 默认的端口号
      */
@@ -17,10 +19,13 @@ public class BIOServer {
      * 单例的ServerSocket
      */
     private static ServerSocket server;
+    /**
+     * 线程池 懒汉式的单例
+     */
+    private static ExecutorService executorService = Executors.newFixedThreadPool(60);
 
     /**
      * 根据传入参数设置监听端口，如果没有参数调用以下方法并使用默认值
-     *
      * @throws IOException
      */
     public static void start() throws IOException {
@@ -28,12 +33,6 @@ public class BIOServer {
         start(DEFAULT_PORT);
     }
 
-    /**
-     * 这个方法不会被大量并发访问，不太需要考虑效率，直接进行方法同步就行了
-     *
-     * @param port
-     * @throws IOException
-     */
     public synchronized static void start(int port) throws IOException {
         if (server != null) {
             return;
@@ -49,7 +48,7 @@ public class BIOServer {
                 Socket socket = server.accept();
                 //当有新的客户端接入时，会执行下面的代码
                 //然后创建一个新的线程处理这条Socket链路
-                new Thread(new BIOServerHandler(socket)).start();
+                executorService.execute(new BIOServerHandler(socket));
             }
         } finally {
             //一些必要的清理工作
@@ -67,7 +66,7 @@ public class BIOServer {
             @Override
             public void run() {
                 try {
-                    BIOServer.start();
+                    BIOServerBetter.start();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
--- java-knowledge-base/src/main/java/base/io/util/Calculator.java
+++ java-knowledge-base/src/main/java/base/io/util/Calculator.java
@@ -9,6 +9,12 @@ public final class Calculator {
     private final static ScriptEngine jse = new ScriptEngineManager().getEngineByName("JavaScript");
 
     public static Object cal(String expression) throws ScriptException {
+        try {
+            // 可以增加延时保持住IO线程
+            Thread.sleep(100000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
         return jse.eval(expression);
     }
 }
